e52f762a95513dacc8c04879c841e520fb0db51c
Mmaureeny <muthonimaureen4@outlook.com>
2022-10-28 05:02:20 -0700

    amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -16,17 +16,6 @@ def get_amenities():
     return jsonify(list_amenities)
 
 
-@app_views.route("/amenities/<amenity_id>", methods=['GET'],
-                 strict_slashes=False)
-def get_amenity(amenity_id):
-    """ Retrieves an amenity """
-    amenity = storage.get(Amenity, amenity_id)
-    if not amenity:
-        abort(404)
-
-    return jsonify(amenity.to_dict())
-
-
 @app_views.route("/amenities/<amenity_id>", methods=['DELETE'],
                  strict_slashes=False)
 def delete_amenity(amenity_id):
